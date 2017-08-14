c2b760a42c64e3fcbf1d1c20bfd8482e1e96d461
Ricardo Jota <ricardo.jota@tactuallabs.com>
2017-08-13 21:05:09 -0400

    Changed the backtick to tab and aligned the code

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -30,24 +30,6 @@
 #define Macro_Any M(MACRO_ANY)
 #define NUMPAD_KEYMAP 2
 
-#define GENERIC_FN2  KEYMAP_STACKED ( \
-___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,         \
-Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE, \
-Key_PageUp, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,                   \
-Key_PageDown,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE, \
-                                            Key_LeftControl, Key_Delete, Key_LeftGui, Key_LeftShift,  \
-                                                                ___,   \
-\
-Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                  Key_F9,          Key_F10,          Key_F11, \
-Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,   Key_LeftBracket, Key_RightBracket, Key_F12, \
-                            Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,             Key_RightArrow,  ___,              ___, \
-Key_Menu,                   Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,            Key_Backslash,    Key_Pipe, \
-Key_RightShift, Key_RightAlt, Key_Enter, Key_RightControl, \
-___ \
-)
-
-
-
 #define NUMPAD KEYMAP_STACKED  (\
     ___, ___, ___, ___, ___, ___, ___,  \
     ___, ___, ___, ___, ___, ___, ___,  \
@@ -65,19 +47,38 @@ ___ \
     Key_Keymap1_Momentary \
 )
 
-#define QWERTY KEYMAP_STACKED ( \
-    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
-    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
-    Key_LeftControl,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
-    Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
-    Key_Spacebar, Key_Backspace, Key_LeftAlt, Key_Delete,            \
-                          Key_Keymap1_Momentary,     \
+
+#define GENERIC_FN2  KEYMAP_STACKED ( \
+___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,         \
+Key_Backtick,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE, \
+Key_PageUp, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,                   \
+Key_PageDown,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE, \
+Key_Spacebar, Key_Backspace, Key_LeftAlt, Key_Delete,            \
+                                                                ___,   \
 \
-    Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
-    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
-                   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
-    Key_LeftGui,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
-    Key_RightShift, Key_LeftShift, Key_RightControl, Key_Enter,                   \
+Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                  Key_F9,          Key_F10,          Key_F11, \
+Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,   Key_LeftBracket, Key_RightBracket, Key_F12, \
+                            Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,             Key_RightArrow,  ___,              ___, \
+Key_Menu,                   Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,            Key_Backslash,    Key_Pipe, \
+Key_RightShift, Key_LeftShift, Key_RightControl, Key_Enter,                   \
+___ \
+)
+
+#define QWERTY KEYMAP_STACKED ( \
+    ___,             Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
+    Key_Tab,         Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
+    Key_LeftControl, Key_A, Key_S, Key_D, Key_F, Key_G,                    \
+    Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
+    \
+    Key_Spacebar, Key_Backspace, Key_LeftAlt, Key_Delete,                  \
+    Key_Keymap1_Momentary,\
+\
+    Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Equals,    \
+    Key_Enter,       Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Minus,     \
+                     Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,     \
+    Key_LeftGui,     Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_RightShift,\
+    \
+    Key_RightShift, Key_LeftShift, Key_RightControl, Key_Enter,                         \
     Key_Keymap1_Momentary \
 )
 
